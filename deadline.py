3d708d0d5d7cda9616d40a3b668d7f8b70314dbf
contact9853 <contact9853@gmail.com>
2020-04-11 10:24:41 +0900

    dealine check

--- deadline.py
+++ deadline.py
@@ -3,34 +3,33 @@ import datetime
 
 url = 'http://worldclockapi.com/api/json/utc/now'
 
-def isDealine():
+def isDeadline():
 
-    deadline = datetime.datetime(2020, 4, 12)
+    deadline = datetime.datetime(2020, 4, 11)
     print(deadline)
 
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        content = resp.json()
-        current = content.get('currentDateTime', '')
-        if current:
-            cur_list = current.split('-')
-            print(cur_list)
+    status = 'ERROR'
+    try:
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            content = resp.json()
+            current = content.get('currentDateTime', '')
+            if current:
+                cur_list = current.split('-')
+                print(cur_list)
 
-            remote   = datetime.datetime(int(cur_list[0]), int(cur_list[1]), int(cur_list[2][:2]))
-            print('remote {} >= deadline {}' .format(remote, deadline))
+                remote   = datetime.datetime(int(cur_list[0]), int(cur_list[1]), int(cur_list[2][:2]))
+                print('remote {} >= deadline {}' .format(remote, deadline))
 
-            if remote >= deadline :
-                print('stop')
-                return 'NG'
-            else:
-                print('go')
-                return 'OK'
-        else:
-            print('info error')
-            return 'ERROR'
-    else:
-        print('net error')
-        return 'ERROR'
+                if remote >= deadline :
+                    print('stop')
+                    return 'NG'
+                else:
+                    print('go')
+                    return 'OK'
+        return status
+    except Exception as ex:
+        return status
 
 
 if  __name__ == '__main__' :
